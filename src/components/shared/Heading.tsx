0d5d4373c335fde2de71f5bcfd34701402d99091
WebSculptor <abdullahisalihuinusa@gmail.com>
2023-12-06 14:15:35 +0100

    Update Heading.tsx

--- src/components/shared/Heading.tsx
+++ src/components/shared/Heading.tsx
@@ -13,7 +13,9 @@ export default function Heading({
     <div className="flex items-end justify-between gap-6 flex-wrap">
       <div className="space-y-2">
         <p className="pl-3 border-l border-blue-500 text-sm">{subtitle}</p>
-        <h1 className="text-3xl md:text-4xl font-black">{title}</h1>
+        <h1 className="w-full lg:max-w-xl text-2xl md:text-3xl lg:text-4xl font-black">
+          {title}
+        </h1>
         {description && (
           <p className="text-[15px] w-full md:max-w-2xl pt-1 text-muted-foreground">
             {description}
