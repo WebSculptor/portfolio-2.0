279943ed03504be72b89f7c34fb293b6c9f25858
WebSculptor <abdullahisalihuinusa@gmail.com>
2023-12-06 14:13:31 +0100

    Update Changelog.tsx

--- src/components/shared/Changelog.tsx
+++ src/components/shared/Changelog.tsx
@@ -2,11 +2,11 @@ export default function Changelog() {
   return (
     <div className="space-y-2">
       <div>
-        <p className="text-base text-muted-foreground flex items-center gap-2">
+        <p className="text-sm text-muted-foreground flex items-center gap-2">
           <span className="w-[72px]">Released:</span> 20 May, 2023
         </p>
 
-        <p className="text-base text-muted-foreground flex items-center gap-2">
+        <p className="text-sm text-muted-foreground flex items-center gap-2">
           <span className="w-[72px]">Update:</span> 25 May, 2023
         </p>
       </div>
