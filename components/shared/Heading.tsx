764f37b9fd12e99965158c712837fec58b95d9a9
WebSculptor <abdullahisalihuinusa@gmail.com>
2023-12-19 14:52:12 +0100

    Update Heading.tsx

--- components/shared/Heading.tsx
+++ components/shared/Heading.tsx
@@ -2,7 +2,7 @@ export default function Heading({ title, subtitle }: HeadingPropType) {
   return (
     <div className="my-6 space-y-2">
       <h2 className="w-max text-2xl font-semibold">{title}</h2>
-      <p className="max-w-xl text-base leading-relaxed text-muted-foreground">
+      <p className="max-w-xl text-sm md:text-base leading-relaxed text-muted-foreground">
         {subtitle}
       </p>
     </div>
